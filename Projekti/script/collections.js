929b1ad9516c32d7674415849ad6a0908e49e024
dafinamuhaxheri <50775189+dafinamuhaxheri@users.noreply.github.com>
2021-01-01 21:56:09 +0100

    Create collections.js

---
+++ Projekti/script/collections.js
@@ -0,0 +1,60 @@
+
+function fotot()
+    {  
+    var x = location.hash;
+    document.getElementsByClassName("Zara")[0].style.display = "block";
+    document.getElementsByClassName("Mango")[0].style.display = "none";
+    document.getElementById("Mango").style.display="none";
+    document.getElementsByClassName("Hotic")[0].style.display = "none";
+    document.getElementById("Hotic").style.display="none";
+    document.getElementsByClassName("Stradivarius")[0].style.display = "none";
+    document.getElementById("Stradivarius").style.display="none";
+    }
+
+      function Zara()
+    {  
+    document.getElementById("Zara").style.display="block";
+    document.getElementsByClassName("Zara")[0].style.display = "block";
+    document.getElementsByClassName("Mango")[0].style.display = "none";
+    document.getElementById("Mango").style.display="none";
+    document.getElementsByClassName("Hotic")[0].style.display = "none";
+    document.getElementById("Hotic").style.display="none";
+    document.getElementsByClassName("Stradivarius")[0].style.display = "none";
+    document.getElementById("Stradivarius").style.display="none";
+    }
+        function Mango()
+    {  
+    document.getElementById("Zara").style.display="none";
+    document.getElementsByClassName("Zara")[0].style.display ="none"
+    document.getElementsByClassName("Mango")[0].style.display = "block";
+    document.getElementById("Mango").style.display="block";
+    document.getElementsByClassName("Hotic")[0].style.display = "none";
+    document.getElementById("Hotic").style.display="none";
+    document.getElementsByClassName("Stradivarius")[0].style.display = "none";
+    document.getElementById("Stradivarius").style.display="none";
+    }
+     function Stradivarius()
+    {  
+    document.getElementById("Zara").style.display= "none";
+    document.getElementsByClassName("Zara")[0].style.display =  "none";
+    document.getElementsByClassName("Mango")[0].style.display = "none";
+    document.getElementById("Mango").style.display="none";
+    document.getElementsByClassName("Hotic")[0].style.display = "none";
+    document.getElementById("Hotic").style.display="none";
+    document.getElementsByClassName("Stradivarius")[0].style.display = "block";
+    document.getElementById("Stradivarius").style.display="block";
+    }
+     function Hotic()
+    {  
+    document.getElementById("Zara").style.display="none";
+    document.getElementsByClassName("Zara")[0].style.display = "none";
+    document.getElementsByClassName("Mango")[0].style.display = "none";
+    document.getElementById("Mango").style.display="none";
+    document.getElementsByClassName("Hotic")[0].style.display = "block";
+    document.getElementById("Hotic").style.display="block";
+    document.getElementsByClassName("Stradivarius")[0].style.display = "none";
+    document.getElementById("Stradivarius").style.display="none";
+    }
+
+
+
