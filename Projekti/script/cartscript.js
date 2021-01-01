3d739fd97f457f62f4ad7e7cebdee79277ef1b31
Arlinda Osmani <45092712+arlindaosmani@users.noreply.github.com>
2021-01-01 22:42:49 +0100

    Add files via upload

---
+++ Projekti/script/cartscript.js
@@ -0,0 +1,184 @@
+
+
+function fotot() {
+     var x = location.hash;
+
+     if (x == "#1") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/zara/z11.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$18,055.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "18055";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "18055";
+          document.getElementsByClassName("brand")[0].innerHTML = "Zara";
+          document.getElementsByClassName("name")[0].innerHTML = "Metalic Pink";
+     }
+     else if (x == "#2") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/zara/z1.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$12,995.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "12995";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "12995";
+          document.getElementsByClassName("brand")[0].innerHTML = "Zara";
+          document.getElementsByClassName("name")[0].innerHTML = "Black Leather";
+     }
+
+     else if (x == "#3") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/zara/z2.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$5755,5.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "5755.5";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "5755.5";
+          document.getElementsByClassName("brand")[0].innerHTML = "Zara";
+          document.getElementsByClassName("name")[0].innerHTML = "Modern Pastels";
+     }
+
+     else if (x == "#4") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/zara/z3.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$7,995.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "7995";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "7995";
+          document.getElementsByClassName("brand")[0].innerHTML = "Zara";
+          document.getElementsByClassName("name")[0].innerHTML = "Classy Red";
+     }
+
+     else if (x == "#5") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/mango/m1.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$43,110.0";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "43110";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "43110";
+          document.getElementsByClassName("brand")[0].innerHTML = "Mango";
+          document.getElementsByClassName("name")[0].innerHTML = "Snake Print";
+     }
+
+     else if (x == "#6") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/mango/m2.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$93,525.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "93525";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "93525";
+          document.getElementsByClassName("brand")[0].innerHTML = "Mango";
+          document.getElementsByClassName("name")[0].innerHTML = "Modern Mustard";
+     }
+
+     else if (x == "#7") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/mango/m3.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$1,170,000.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "1170000";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "1170000";
+          document.getElementsByClassName("brand")[0].innerHTML = "Mango";
+          document.getElementsByClassName("name")[0].innerHTML = "Chique Black";
+     }
+
+     else if (x == "#8") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/mango/m4.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$214,890.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "214890";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "214890";
+          document.getElementsByClassName("brand")[0].innerHTML = "Mango";
+          document.getElementsByClassName("name")[0].innerHTML = "Gen Z Lilac";
+     }
+
+
+     else if (x == "#9") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/stradivarius/s1.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$23,715.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "23715";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "23715";
+          document.getElementsByClassName("brand")[0].innerHTML = "Stradivarius";
+          document.getElementsByClassName("name")[0].innerHTML = "Red Leather";
+     }
+
+
+     else if (x == "#10") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/stradivarius/s2.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$13,221.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "13221";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "13221";
+          document.getElementsByClassName("brand")[0].innerHTML = "Stradivarius";
+          document.getElementsByClassName("name")[0].innerHTML = "Black Bag";
+     }
+
+
+     else if (x == "#11") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/stradivarius/s3.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$50,908.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "50908";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "50908";
+          document.getElementsByClassName("brand")[0].innerHTML = "Stradivarius";
+          document.getElementsByClassName("name")[0].innerHTML = "Brown Handbag";
+     }
+
+     else if (x == "#12") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/stradivarius/s4.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$38,024.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "38024";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "38024";
+          document.getElementsByClassName("brand")[0].innerHTML = "Stradivarius";
+          document.getElementsByClassName("name")[0].innerHTML = "Purple Clothed";
+     }
+
+     else if (x == "#13") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/hotic/h1.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$36,486.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "36486";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "36486";
+          document.getElementsByClassName("brand")[0].innerHTML = "Hotic";
+          document.getElementsByClassName("name")[0].innerHTML = "Young Black";
+     }
+
+     else if (x == "#14") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/hotic/h2.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$21,870.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "21870";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "21870";
+          document.getElementsByClassName("brand")[0].innerHTML = "Hotic";
+          document.getElementsByClassName("name")[0].innerHTML = "Handheld Brown";
+     }
+
+     else if (x == "#15") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/hotic/gh.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$53,910.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "53910";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "53910";
+          document.getElementsByClassName("brand")[0].innerHTML = "Hotic";
+          document.getElementsByClassName("name")[0].innerHTML = "Dark Green Bag";
+     }
+
+     else if (x == "#16") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/bags/hotic/h4.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$32,715.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "32715";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "32715";
+          document.getElementsByClassName("brand")[0].innerHTML = "Hotic";
+          document.getElementsByClassName("name")[0].innerHTML = "White Handheld";
+     }
+}
+
